a4ce57b72c41ac2984f92788ad27904eebc724fe
franznkemaka <franz@rigle.co>
2024-01-30 11:01:19 +0100

    ✨ strings

--- code/basics/calculator.py
+++ code/basics/calculator.py
@@ -3,14 +3,13 @@ num1 = float(input("enter number 1 = "))
 num2 = float(input("enter number 2 = "))
 
 
-addition = num1 + num2
-subtraction = num2 -num1
-multiplication = num1 * num2
-division = num1 / num2 
+addition = round(num1 + num2, 2)
+subtraction = round(num2 -num1, 2)
+multiplication = round(num1 * num2, 2)
+division = round(num1 / num2, 2)
 
 
 print(f"Addition = {addition}")
 print(f"Subtraction = {subtraction}")
 print(f"Division = {division}")
 print(f"Multiplication = {multiplication}")
-
---
+++ code/basics/strings.py
@@ -0,0 +1,36 @@
+name  = "franz test"
+
+print(f"User input: {name}")
+
+# replace a portion
+name = name.replace("test", "")
+print(f"name without test: {name}")
+
+# remove any whitespace
+name = name.strip()
+
+# make the first letter upper
+name = name.capitalize()
+print(f"capitalize name: {name}")
+
+
+name_substring  = name[0:2]
+print(f"First 2 characters are: {name_substring}")
+
+name_substring  = name[-2:]
+print(f"Last 2 characters are: {name_substring}")
+
+print(f"your has {len(name)} characters!")
+print(f"Uppercase: {name.upper()}")
+print(f"Lowercase: {name.lower()}")
+
+
+# check if name has whitespace
+if " " in name: 
+    print("Please remove the whitespace from your name")
+
+# check if "a" in name
+if "a" in name: 
+    print('Your name has an "a" ' )
+else: 
+    print("Your name doesn't have an 'a'")
