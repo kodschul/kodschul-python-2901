32a6c91359ec5141c885401aa415eb93cb6ca52e
franznkemaka <franz@rigle.co>
2024-01-30 09:38:15 +0100

    ✨

---
+++ code/basics/calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("enter number 1 = "))
+num2 = float(input("enter number 2 = "))
+
+
+addition = num1 + num2
+subtraction = num2 -num1
+multiplication = num1 * num2
+division = num1 / num2 
+
+
+print(f"Addition = {addition}")
+print(f"Subtraction = {subtraction}")
+print(f"Division = {division}")
+print(f"Multiplication = {multiplication}")
+
--- code/basics/greet.py
+++ code/basics/greet.py
@@ -1,8 +1,14 @@
 name = input("What's your name? ")
 age_str = input("How old are you? ")
+is_celebrated_str = input("Did you celebrate your birthday already? (Y/N) ")
 
 age = int(age_str)
-birth_year = 2024 - age
+
+if is_celebrated_str == "Y":
+    birth_year = 2024 - age
+else: 
+    birth_year = 2024 - age - 1
+    
 
 message = "Hi " + name 
 message += ", you are " + age_str 
