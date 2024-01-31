f61460d76422366e236e36298019cf9ea4fabb2a
franznkemaka <franz@rigle.co>
2024-01-31 16:26:35 +0100

    ✨

---
+++ code/basics/functions.py
@@ -0,0 +1,27 @@
+
+
+def add(num1, num2):
+    answer = num1 + num2
+
+    print("test")
+    return answer
+
+
+output = add(12, 2)
+
+
+print(output)
+
+
+# def greet(name="NoName", age=100, hobbies=None):
+#     print(f"Hi {name}, {age} y/o")
+
+#     if hobbies:
+#         print("Cool, you have hobbies!")
+
+
+# greet(age=20, hobbies="Coding", name="Franz")
+# # greet("Franz", 20)
+# greet("Mike", 21)
+# greet("Michael", 40)
+# greet()
