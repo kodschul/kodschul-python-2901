ef490b208096de498f1733abbe91c327bedcdcd2
franznkemaka <franz@rigle.co>
2024-02-01 16:44:34 +0100

    ✨

---
+++ code/advanced/classes.py
@@ -0,0 +1,18 @@
+class Animal:
+
+    def __init__(self, name, age):
+        print(f"A new animal was born, {name} ")
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hi {self.name}, you're {self.age} y/o")
+
+
+print("Animal class")
+
+dog = Animal("dog", 5)
+cat = Animal("cat", 3)
+
+dog.greet()
+cat.greet()
