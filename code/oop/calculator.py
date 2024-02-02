b8f362d4a49ace31463cf695a336d30033157f68
franznkemaka <franz@rigle.co>
2024-02-02 09:43:08 +0100

    ✨  oop

---
+++ code/oop/calculator.py
@@ -0,0 +1,8 @@
+
+
+calculator = Calculator()
+
+print(f"Addition = {calculator.add(1,2)}")
+print(f"Subtraction = {calculator.subtract(3,2)}")
+print(f"Division = {calculator.divide(5,2)}")
+print(f"Multiplication = {calculator.multiply(5, 5)}")
