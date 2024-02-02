66435df0e1c91f2b412ebcc7ac5e4cfa44c4fbd5
franznkemaka <franz@rigle.co>
2024-02-02 10:09:21 +0100

    ✨ bank

---
+++ code/oop/bank.py
@@ -0,0 +1,18 @@
+# your class comes here! ....
+
+
+account1 = BankAccount()
+
+
+# print: Your current balance is: 0€
+account1.show_balance()
+
+account1.deposit(100)
+
+# pint: Your current balance is: 100€
+account1.show_balance()
+
+account1.withdraw(50)
+
+# pint: Your current balance is: 50€
+account1.show_balance()
