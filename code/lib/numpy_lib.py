099a5443dee079632494e0209c929456e7acd40f
franznkemaka <franz@rigle.co>
2024-02-02 14:20:12 +0100

    ✨ numpy

---
+++ code/lib/numpy_lib.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+nums1 = [1, 2, 3]
+nums2 = [2, 4, 6]
+
+# convert to a numpy array to process it
+np1 = np.array(nums1)
+np2 = np.array(nums2)
+
+
+# sum
+addition = np1 + np2
+subtraction = np1 - np2
+# ...
+
+print(addition)
+print(subtraction)
