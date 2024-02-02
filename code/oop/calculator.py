dc5b5f188f16cfc6e0a8e74e097eef52ade9a4a2
franznkemaka <franz@rigle.co>
2024-02-02 10:05:47 +0100

    ✨ calculator

--- code/oop/calculator.py
+++ code/oop/calculator.py
@@ -1,5 +1,20 @@
-
 # your class comes here! ....
+
+
+class Calculator:
+
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    def divide(self, num1, num2):
+        return num1 / num2
+
+    def multiply(self, num1, num2):
+        return num1 * num2
+
 
 calculator = Calculator()
 
